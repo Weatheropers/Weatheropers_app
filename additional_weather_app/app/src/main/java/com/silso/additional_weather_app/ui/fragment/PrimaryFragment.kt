41311d03bbafd0f661108fa15fa1b077700e537c
sirasatarato <sirasatarato@gmail.com>
2020-05-12 19:42:18 +0900

    some fix symbol

--- additional_weather_app/app/src/main/java/com/silso/additional_weather_app/ui/fragment/PrimaryFragment.kt
+++ additional_weather_app/app/src/main/java/com/silso/additional_weather_app/ui/fragment/PrimaryFragment.kt
@@ -4,21 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.silso.additional_weather_app.R
+import com.silso.additional_weather_app.data.PrimaryData
+import com.silso.additional_weather_app.databinding.PrimaryListItemBinding
 import com.silso.additional_weather_app.ui.adapter.PrimaryAdapter
 import com.silso.additional_weather_app.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.fragment_primary.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PrimaryFragment : Fragment() {
+    lateinit var binding: PrimaryListItemBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_primary, container, false)
+        binding = DataBindingUtil.inflate<PrimaryListItemBinding>(inflater, R.layout.fragment_primary, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,10 +32,19 @@ class PrimaryFragment : Fragment() {
 
         val viewModel: MainViewModel by viewModel()
         viewModel.primaryLiveData.observe(viewLifecycleOwner, Observer {
-            val lim = LinearLayoutManager(activity)
-            lim.orientation = LinearLayoutManager.HORIZONTAL
-            primary_fragment_recyclerView.layoutManager = lim
-            primary_fragment_recyclerView.adapter = PrimaryAdapter(it)
+            setData(it[0])
+            setList(it)
         })
+    }
+
+    private fun setData(primaryData: PrimaryData) {
+        binding.primaryData = primaryData
+    }
+
+    private fun setList(it: List<PrimaryData>) {
+        val lim = LinearLayoutManager(activity)
+        lim.orientation = LinearLayoutManager.HORIZONTAL
+        primary_fragment_recyclerView.layoutManager = lim
+        primary_fragment_recyclerView.adapter = PrimaryAdapter(it)
     }
 }
--- additional_weather_app/app/src/main/java/com/silso/additional_weather_app/viewmodel/MainViewModel.kt
+++ additional_weather_app/app/src/main/java/com/silso/additional_weather_app/viewmodel/MainViewModel.kt
@@ -2,12 +2,13 @@ package com.silso.additional_weather_app.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.silso.additional_weather_app.data.PrimaryData
 import com.silso.additional_weather_app.data.SchoolData
 import com.silso.additional_weather_app.data.SimpleData
 import com.silso.additional_weather_app.data.TimeData
 
 class MainViewModel(private var repository: WeatherRepository) : ViewModel() {
-    val primaryLiveData: LiveData<List<SimpleData>> = repository.primaryLiveData
+    val primaryLiveData: LiveData<List<PrimaryData>> = repository.primaryLiveData
     val schoolLiveData: LiveData<List<SchoolData>> = repository.schoolLiveData
     val detailLiveData: LiveData<List<SimpleData>> = repository.detailLiveData
     val timezoneLiveData: LiveData<List<TimeData>> = repository.timezoneLiveData
